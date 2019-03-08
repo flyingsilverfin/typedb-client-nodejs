85ce568c2ca435b07544eb5182dd74ef76fdba24
Joshua Send <joshuasend1@gmail.com>
2019-03-08 19:09:43 +0000

    Create body of deployment test

--- tests/deployment/nodeApplicationTest.js
+++ tests/deployment/nodeApplicationTest.js
@@ -2,13 +2,35 @@ const GraknClient = require("grakn-client");
 
 
 beforeEach(async () => {
-    client = GraknClient("localhost:48555");
-    session = client.session("testkeyspace");
+    client = await GraknClient("localhost:48555");
+    session = await client.session("testkeyspace");
     tx = await session.transaction().write();
 })
 
 afterEach(() => {
-    tx.close();
-    session.close();
+    await tx.close();
+    await session.close();
     client.close()
 });
+
+
+
+
+describe("Basic GraknClient Tests", () => {
+
+    test("define", async () => {
+        const defined = await tx.query("define person sub entity, has name; name sub attribute, datatype string;");
+        await tx.commit()
+    });
+
+    test("match", async () => {
+        const types = await tx.query("match $x sub thing; get;");
+    });
+
+    test("insert", async () => {
+        const defined = await tx.query("define person sub entity, has name; name sub attribute, datatype string;");
+        const inserted = await tx.query("insert $x isa person, has name \"john\";");
+    });
+});
+
+
